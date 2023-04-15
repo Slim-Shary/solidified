a60c6b4318c977568dac5de2cd71f4bfaf0d97da
Slim-Shary <shahryarmon@gmail.com>
2023-04-15 12:14:49 -0700

    fetch data on search button click only

--- src/GlobalContext/store.tsx
+++ src/GlobalContext/store.tsx
@@ -10,6 +10,10 @@ import { Repo } from '../components/RepoCard';
 interface ContextProps {
   username: Accessor<string>;
   setUsername: Setter<string>;
+  searchedUser: Accessor<string>;
+  setSearchedUser: Setter<string>;
+  repos: Accessor<Repo[]>;
+  setRepos: Setter<Repo[]>;
   savedRepos: Accessor<Repo[]>;
   setSavedRepos: Setter<Repo[]>;
 }
@@ -19,12 +23,23 @@ const GlobalContext = createContext<ContextProps>();
 export function GlobalContextProvider(props: any) {
   const storedRepos = JSON.parse(localStorage.getItem('savedRepos') || '[]');
 
-  const [username, setUsername] = createSignal('solidjs');
+  const [username, setUsername] = createSignal('');
+  const [searchedUser, setSearchedUser] = createSignal('');
+  const [repos, setRepos] = createSignal<Repo[]>([]);
   const [savedRepos, setSavedRepos] = createSignal<Repo[]>(storedRepos);
 
   return (
     <GlobalContext.Provider
-      value={{ username, setUsername, savedRepos, setSavedRepos }}
+      value={{
+        username,
+        setUsername,
+        searchedUser,
+        setSearchedUser,
+        repos,
+        setRepos,
+        savedRepos,
+        setSavedRepos,
+      }}
     >
       {props.children}
     </GlobalContext.Provider>
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -13,31 +13,69 @@ const fetchRepos = async (username: string) => {
 };
 
 const Home: Component = () => {
-  const { username, setUsername } = useGlobalContext();
-  const [repos] = createResource(username, fetchRepos);
+  const {
+    username,
+    setUsername,
+    searchedUser,
+    setSearchedUser,
+    repos,
+    setRepos,
+  } = useGlobalContext();
+  const [error, setError] = createSignal<Error>();
+  const [loading, setLoading] = createSignal(false);
+
+  const handleSubmit = async (e: Event) => {
+    e.preventDefault();
+    if (username()) {
+      setSearchedUser(username());
+      setLoading(true);
+      try {
+        const res = await fetchRepos(username());
+        setRepos(res);
+        setError(undefined);
+      } catch (err: any) {
+        setError(err);
+      }
+      setLoading(false);
+    } else {
+      setSearchedUser('');
+      setRepos([]);
+    }
+  };
 
   return (
     <>
-      <div class='fetch-container mb-3'>
-        <input
-          type='text'
-          class='form-control search-control'
-          id='usernameInput'
-          value={username()}
-          onInput={(e) => setUsername(e.currentTarget.value)}
-          required
-          placeholder='GitHub username'
-        />
-      </div>
-      <h3>Github repos for {username}</h3>
-      <Show when={!repos.loading} fallback={<div>Loading...</div>}>
-        <Show when={!repos.error} fallback={<div>{repos.error.message}</div>}>
-          <For
-            each={repos()}
-            fallback={<span>No repos have been created yet by this user</span>}
-          >
-            {(repo: Repo) => <RepoCard repo={repo} />}
-          </For>
+      <form onSubmit={handleSubmit}>
+        <div class='fetch-container mb-3'>
+          <input
+            type='text'
+            class='form-control search-control'
+            id='usernameInput'
+            value={username()}
+            onChange={(e) => setUsername(e.currentTarget.value)}
+            placeholder='GitHub username'
+          />
+          <button type='submit' class='btn btn-primary'>
+            Search
+          </button>
+        </div>
+      </form>
+      <Show when={!loading()} fallback={<div>Loading...</div>}>
+        <Show
+          when={!error()}
+          fallback={<p class='text-danger'>{error()?.message}</p>}
+        >
+          <Show when={searchedUser()}>
+            <h3>Github repos for {searchedUser()}</h3>
+            <For
+              each={repos()}
+              fallback={
+                <span>No repos have been created yet by this user</span>
+              }
+            >
+              {(repo: Repo) => <RepoCard repo={repo} />}
+            </For>
+          </Show>
         </Show>
       </Show>
     </>
